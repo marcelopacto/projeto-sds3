5cbb02b6f5f1cc2335d91a90ba5119151fdf76c4
Marcelo Assunção <marcelo@pactosistemas.com.br>
2021-05-03 18:02:06 -0400

    Statics Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable/Index";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -7,9 +9,25 @@ function App() {
     <>
     <NavBar/>
     <div className="container">
-       <h1 className="text-primary">Olá Mundo</h1>
+       <h1 className="text-primary py-3">DashBoard das Vendas</h1>
+
+       <div className="row px-3">
+         <div className="col-sm-6">
+           <h5 className="text-center text-secondary"> Taxa de Sucesso (%)</h5>
+           <BarChart/>
+         </div>
+
+         <div className="col-sm-6">
+           <h5>Todas as Vendas</h5>
+           <DonutChart/>
+         </div>
+       </div>
+<div className="py-3">
+  <h2 className="text-primary">Vendas Concluídas no Período</h2>
+</div>
+
        <DataTable/>
-       
+
     </div>
     <Footer/>
     </>
