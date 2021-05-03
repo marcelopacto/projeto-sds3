d30ade9c9bfb50aafdced2275b220aef1eebd3e5
Marcelo Assunção <marcelo@pactosistemas.com.br>
2021-05-03 16:53:41 -0400

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/marcelopacto" target="_blank" rel="noreferrer">Marcelo Assunção</a></p>
+          <p className="text-light"><small><strong>Reciclando conhecimentos - Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/marcelo@pactosistemas.com.br" target="_blank" rel="noreferrer">@marceloassuncao</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer
+  
