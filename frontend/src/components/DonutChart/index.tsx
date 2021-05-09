d94de0e62188aaeda9bdcac3d32ce34e592ab2df
Marcelo Assunção <marcelo@pactosistemas.com.br>
2021-05-09 18:51:46 -0400

    First Request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,11 +1,34 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+ type ChartData = {
+     labels:string [];
+     series: number[];
+ }
 
 const DonutChart = () => {
     
-    const mockData = {
-        series: [137132.39, 41797.34, 99440.83, 25393.83],
-        labels: ['Easy Alpha', 'Easy Primavera', 'Easy Lucas', 'EasyN.Mutum']
-    }
+    //  FORMA ERRADA  PORQUE CONSUME A API DE DADOS VÁRIAS VEZES ENQUANTO RENDERIZA O GRAFICO
+    let chartData : ChartData = {labels: [], series: []};
+    
+    axios.get(`${BASE_URL}/sales/amout-by-seller`)
+         .then(response => {
+             const data = response.data as SaleSum[];
+             const myLabels = data.map(x => x.sellerName);
+             const mySeries = data.map(x => x.sum);
+
+             chartData = {labels: myLabels, series:mySeries};
+            console.log(response.data);
+
+         }); 
+    // FIM DA FORMA ERRADA
+
+    //const mockData = {
+    //    series: [137132.39, 41797.34, 99440.83, 25393.83],
+    //    labels: ['Easy Alpha', 'Easy Primavera', 'Easy Lucas', 'EasyN.Mutum']
+    //}
     
     const options = {
         legend: {
@@ -14,8 +37,8 @@ const DonutChart = () => {
     }
     return (
       <Chart 
-       options = {{...options, labels: mockData.labels}}
-       series={mockData.series}
+       options = {{...options, labels: chartData.labels}}
+       series={chartData.series}
        type = "donut"
        height = "240"
       />
