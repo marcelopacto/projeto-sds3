c5732deb99c2e33bedb2280af251b053bfa8c940
Marcelo Assunção <marcelo@pactosistemas.com.br>
2021-05-10 09:04:29 -0400

    Datatable integration

--- frontend/src/components/DataTable/Index.tsx
+++ frontend/src/components/DataTable/Index.tsx
@@ -1,63 +1,52 @@
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "utils/requests";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+
 const DataTable = () => {
+
+    const [page, setPage] = useState <SalePage>({
+        first: true,
+        last:true,
+        number:0,
+        totalElements:0,
+        totalPages:0
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=2&size=30&sort=date,desc`)
+        .then(response => {setPage(response.data)
+        }); 
+    }, []);
+
+
+
     return (
         <div className="table-responsive">
     <table className="table table-striped table-sm">
         <thead>
             <tr>
-                <th>Loja</th>
-                <th>Qtde Vendas</th>
-                <th>Venda Bruta</th>
-                <th>Descontos</th>
-                <th>Venda Líquida</th>
-                <th>Ticket Médio</th>
+                <th>Data</th>
+                <th>Vendedor</th>
+                <th>Clientes Visitados</th>
+                <th>Negócios Fechados</th>
+                <th>Valor</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Easy Alpha</td>
-                <td>129</td>
-                <td>144.032,54</td>
-                <td>  6.900,15</td>
-                <td>137.132,39</td>
-                <td>  1.063,04</td>
-                <td>Easy Alpha</td>
+            {page.content?.map(item => (
+                <tr key={item.id}>
+                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>    
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
             </tr>
-            <tr>
-                <td>Easy Primavera</td>
-                <td> 40</td>
-                <td>43.027,34</td>
-                <td> 1.230,00</td>
-                <td>41.797,34</td>
-                <td> 1.044,93</td>
-                <td>Easy Primavera</td>
-            </tr>
-            <tr>
-                <td>Easy Lucas</td>
-                <td> 66</td>
-                <td>333.424,80</td>
-                <td>233.983,97</td>
-                <td> 99.440,83</td>
-                <td>  1.506,68</td>
-                <td>Easy Lucas</td>
-            </tr>
-            <tr>
-                <td>Easy N.Mutum</td>
-                <td> 14</td>
-                <td>66.420,00</td>
-                <td>41.026,17</td>
-                <td>25.393,83</td>
-                <td> 1.813,85</td>
-                <td>Easy N.Mutum</td>
-            </tr>
-            <tr>
-                <td><h5>TOTAL</h5></td>
-                <td><h5>249</h5></td>
-                <td><h5>586.904,68</h5></td>
-                <td><h5>283.140,29</h5></td>
-                <td><h5>183.764,39</h5></td>
-                <td><h5>  1.357,13</h5></td>
-                <td><h5>TOTAL</h5></td>
-            </tr>
+            ))}
+            
+            
         </tbody>
     </table>
 </div>
