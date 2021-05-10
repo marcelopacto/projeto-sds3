d269ee9cb1156fe6ff73aae4a7979964bfd69f67
Marcelo Assunção <marcelo@pactosistemas.com.br>
2021-05-10 17:24:37 -0400

    Env Var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8080';
+// SE BACKEND_URL não estiver definida na hospedagem da nuvem (netlify) vai rodar localmente na 8080
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL?? 'http://localhost:8080';
